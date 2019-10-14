5ad4908f507fab232374e6d1587dd3925b9b44a1
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-10-14 11:59:30 +0200

    add literasls and control structures

---
+++ src/literals.js
@@ -0,0 +1,4 @@
+let message = "Hello World" //String-Literal
+let number = 42 //Zahlen-Literal
+let realNumber = 47.11 //Zahlen-Literal
+let logicState = true //boolsche literale true|false
---
+++ src/simpleControllStructures.js
@@ -0,0 +1,21 @@
+//JavaScript: In erster Näherung ganz normal!
+
+const counter = 5
+
+for (let i = 0; i < counter; i++){
+    console.log("Index: " + i)
+}
+
+let shouldContinue = true
+
+
+while(shouldContinue){
+    let index = 0
+    console.log("continuing")
+    if (index == counter){
+        shouldContinue = false
+    }{
+        index++
+    }
+}
+
