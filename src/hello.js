083bd40169489da0521e3c6ca446e3b206e1c668
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-10-14 11:42:40 +0200

    add hello.html

--- src/hello.js
+++ src/hello.js
@@ -1,6 +1,6 @@
 //Deklaration: let|const <name> = Literal
 //message= Referenz auf ein Objekt, "": String-Literal
-const message = "Hello World!"
+const message = "Hello World!!!!"
 let variableMessage = "Hello"
 variableMessage = variableMessage + " World"
 //Referenz.Eigenschaft
