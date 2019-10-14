a8d2dad73a7dbdde4e929ffac20733f80078aed3
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-10-14 14:55:44 +0200

    add Constructor sample

--- src/PersonConstructor.js
+++ src/PersonConstructor.js
@@ -1,13 +1,15 @@
+"use strict"
 function Person(l, f, g, h) {
         this.lastname= l
-        this.f = f 
+        this.firstname = f 
         this.gender = g
         this.height = h
         this.info= function(){
             return `Lastname: ${this.lastname}, Firstname: ${this.firstname}`
         }, 
     
-        this.infoAsJSON = function(){
+        this.infoAsJSON = () =>{
             return JSON.stringify(this)
         }
+        //Object.freeze(this)
 }
