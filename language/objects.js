9ef24d5fadc8c24c40fbe01755ac6f0cca94b4d8
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-02-21 14:19:48 +0100

    play around with constructors and prototypes

---
+++ language/objects.js
@@ -0,0 +1,36 @@
+let person = {}
+//Objekt sind ohne weitere Aktionen dynamisch
+person.lastname = "Sawitzki"
+person.firstname = "Rainer"
+person.greet = function (){ return `Hello, my name is ${this.lastname}`} //TODO: Später hierzu ein Detail
+//console.log(person.greet())
+
+
+let person2 = {
+    lastname: "Mustermann",
+    firstname: "Rainer",
+    greet: function (){ return `Hello, my name is ${this.lastname}`} //TODO: Später hierzu ein Detail
+}
+//console.log(person2.greet())
+
+
+function Person(lastname, firstname){
+    this.lastname = lastname
+    this.firstname = firstname
+       
+}
+
+Person.prototype = {
+    greet: function (){ return `Hello, my name is ${this.lastname}`} //TODO: Später hierzu ein Detail
+
+}
+
+let p1 = new Person("Schneider", "Hanna")
+let p2 = new Person("Meier", "Josef")
+p1.height = 177
+p1.greet = function(){return "egal"}
+console.log(p1.greet())
+console.log(p2.greet())
+
+Person.prototype.demo = function(){return "from demo"}
+console.log(p1.demo())
