f1317401ecd026f1c68cc7bb7c5fe191d0a6850f
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-02-22 09:37:37 +0100

    finish language

--- language/object_util.js
+++ language/object_util.js
@@ -1,12 +1,15 @@
 Object.prototype.getAllProperties = function(){
     const ownProperties = Object.getOwnPropertyNames(this)
     const prototypeProperties = Object.getOwnPropertyNames(this.constructor.prototype)
-    return new Set(ownProperties.concat(prototypeProperties))
+    return new Set(ownProperties.concat(prototypeProperties).sort())
+}
+Object.prototype.getAllConstructorProperties = function(){
+    return new Set(Object.getOwnPropertyNames(this.constructor).sort())
 }
 Object.prototype.getAllMethods = function(){
     const ownProperties = Object.getOwnPropertyNames(this)
     const prototypeProperties = Object.getOwnPropertyNames(this.constructor.prototype)
-    return new Set(ownProperties.concat(prototypeProperties).filter(name => typeof this[name] == 'function'))
+    return new Set(ownProperties.concat(prototypeProperties).filter(name => typeof this[name] == 'function').sort())
 }
 Object.prototype.getAllAttributes = function(){
     const ownProperties = Object.getOwnPropertyNames(this)
@@ -14,7 +17,9 @@ Object.prototype.getAllAttributes = function(){
     return new Set(ownProperties.concat(prototypeProperties).filter(name => typeof this[name] != 'function'))
 }
 
-console.log("Hugo".getAllProperties())
-console.log([].getAllProperties())
-console.log([].getAllMethods())
-console.log([].getAllAttributes())
+//console.log(Object.getOwnPropertyNames([]))
+//console.log([].getAllProperties())
+//console.log([].getAllMethods())
+//console.log([].getAllAttributes())
+//console.log([].getAllConstructorProperties())
+console.log("Hugo".getAllMethods())
