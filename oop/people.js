8f24802ac79e583fa36a6e7583eb43cc1d6fb5e8
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-02-22 10:06:38 +0100

    play around with classes

--- oop/people.js
+++ oop/people.js
@@ -1,7 +1,9 @@
-class Person{
+let Person = class {
+    static number_of_people = 0
     constructor(lastname, firstname){
         this.lastname = lastname
         this.firstname = firstname
+            Person.number_of_people++
     }
     greet(){
         return `Hello, my name is ${this.lastname}`
@@ -9,5 +11,19 @@ class Person{
 }
 
 let p = new Person("Sawitzki", "Rainer")
+let p2 = new Person("Mustermann", "Hannah")
+//console.log(p)
+//console.log(p.greet())
+//console.log(typeof p)
+//console.log(p.constructor)
+
+//p.height = 183
+//delete p.lastname
+
 console.log(p)
-console.log(p.greet())
+console.log(p.constructor)
+console.log(Person.number_of_people)
+Person.prototype.say_hi = function(){return "Hi"}
+console.log(p.say_hi())
+
+
